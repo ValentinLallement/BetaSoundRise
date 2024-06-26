b3925dedd0670dda5748b171509080c019337c1a
Robin Chaillou <90150299+desboisGIT@users.noreply.github.com>
2024-06-26 15:43:56 +0200

    secured secret keysettings.py

--- Soundrise/Soundrise/settings.py
+++ Soundrise/Soundrise/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y-fzh5kfn1jm-i_)8s+d^kk_@#ehtuzb+m@jus74!5!)7ezga2'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
